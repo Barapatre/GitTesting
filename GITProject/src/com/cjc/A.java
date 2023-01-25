29f484ac6cb2f9ce03048ab22152e9300f5b5be9
Siddhesh <sbarapatre7494@gmail.com>
2023-01-25 12:58:23 +0530

    By Ujwala

---
+++ GITProject/src/com/cjc/A.java
@@ -0,0 +1,16 @@
+package com.cjc;
+
+public class A {
+
+	public void m1()
+	{
+	System.out.println("Hello Java");
+	}
+	
+	public static void main(String[] args) {
+	
+	System.out.println("hello");
+
+	}
+
+}
