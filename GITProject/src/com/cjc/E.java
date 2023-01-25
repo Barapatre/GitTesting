81213c34b917f429bd9b8df9bbef1b28eadc4d04
Barapatre <123442166+Barapatre@users.noreply.github.com>
2023-01-25 16:02:47 +0530

    By Chetan

--- GITProject/src/com/cjc/E.java
+++ GITProject/src/com/cjc/E.java
@@ -1,7 +1,11 @@
 package com.cjc;
 
 public class E {
-
+	public void m()
+	{
+	System.out.println("Chiu");
+	System.out.println("Ujwala")
+	}
 	public static void main(String[] args) {
 	System.out.println("I love my Daughter");
 	
