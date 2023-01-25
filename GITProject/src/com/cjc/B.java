e0a124a4ec1eab9884e2275735d64e4e4637cb43
Barapatre <123442166+Barapatre@users.noreply.github.com>
2023-01-25 13:10:44 +0530

    Update B.java

--- GITProject/src/com/cjc/B.java
+++ GITProject/src/com/cjc/B.java
@@ -8,6 +8,10 @@ public class B {
 		{
 			System.out.println("Selenium world");
 		}
+		public void DemoWebShop()
+		{
+		System.out.println("DemoWEb Pages");
+		}
 		public static void main(String[] args) {
 			
 			System.out.println("Call Selenium");
