43365d3095a77e68e354067e7983b0b8717228a5
Siddhesh <sbarapatre7494@gmail.com>
2023-01-25 14:22:33 +0530

    By abc

---
+++ GITProject/src/com/cjc/C.java
@@ -0,0 +1,15 @@
+package com.cjc;
+
+public class C {
+	
+	public void Family()
+	{
+		System.out.println("Enjoy with Family");
+	}
+	public static void main(String[] args) {
+	
+		System.out.println("I Love My Family");
+
+	}
+
+}
