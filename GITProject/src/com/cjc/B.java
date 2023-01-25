f06266eff7df4c0e5219f3a84caa44a2c21a4eed
Siddhesh <sbarapatre7494@gmail.com>
2023-01-25 13:05:01 +0530

    By Chetan

---
+++ GITProject/src/com/cjc/B.java
@@ -0,0 +1,17 @@
+package com.cjc;
+
+public class B {
+
+	
+	
+		public void m1()
+		{
+			System.out.println("Selenium world");
+		}
+		public static void main(String[] args) {
+			
+			System.out.println("Call Selenium");
+
+	}
+
+}
